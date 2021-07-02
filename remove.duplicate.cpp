976d7fec7153aab2d69d9ad9c9ea676ccbf28c8c
Aastha Bhardwaj <63861931+aastha-ML@users.noreply.github.com>
2021-07-03 01:29:16 +0530

    Add files via upload

---
+++ remove.duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+    
+        for(int x = 0; x < nums.size(); x++) {
+            if(nums[x] != val) {
+                nums.insert(nums.begin() + count, nums[x]);
+                count++;
+                x++;
+            }
+        }
+        return count;
+    }
+    };
